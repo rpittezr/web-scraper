550241bbc954324861a3c561b5547603f584b7f7
rpittezr <pittezr.dev@gmail.com>
2024-11-11 16:26:16 -0300

    refact: tasks and modelling

--- src/config/celery.py
+++ src/config/celery.py
@@ -10,21 +10,3 @@ app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
-
-app.conf.beat_schedule = {
-    "multiply-task-crontab": {
-        "task": "multiply_two_numbers",
-        "schedule": crontab(hour=9, minute=30, day_of_week=1),
-        "args": (16, 16),
-    },
-    "multiply-every-5-seconds": {
-        "task": "multiply_two_numbers",
-        "schedule": 5.0,
-        "args": (16, 16),
-    },
-    "add-every-30-seconds": {
-        "task": "movies.tasks.add",
-        "schedule": 30.0,
-        "args": (16, 16),
-    },
-}
